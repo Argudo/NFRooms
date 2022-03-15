50b4272f72fda0e574a7a812526692ef689eba15
Argudo <josexerezano7@gmail.com>
2022-03-15 20:26:11 +0100

    v0.4 Implementacion de JS

---
+++ js/salas.js
@@ -0,0 +1,18 @@
+function cargarInformacion(nombre, cap, precio){
+    let popup = document.getElementById("popup");
+
+    let obj_nombre = document.getElementById("nombre");
+    let obj_precio = document.getElementById("precio");
+    let obj_capacidad = document.getElementById("capacidad");
+
+    obj_nombre.innerHTML = nombre;
+    obj_precio.innerHTML = precio;
+    obj_capacidad.innerHTML = cap;
+
+    popup.setAttribute("style","display: flex;");
+}
+
+function cerrarPopUp(){
+    let popup = document.getElementById("popup");
+    popup.setAttribute("style", "display: none");
+}
