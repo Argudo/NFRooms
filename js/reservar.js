d07be7837c31c1850d04052ef2fbd5479c078690
jagc55 <juanantonio.gonzalezcantero@alum.uca.es>
2022-03-19 19:19:17 +0100

    "Salas cargadas en Select de reservar.html"

--- js/reservar.js
+++ js/reservar.js
@@ -35,7 +35,14 @@ let salas=[
         'descripcion': "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
     }
 ];
+function cargarSalas(){
+    salas.forEach(a);
+}
 
+function a(sala){
+    console.log(salas);
+    $('#selectorSala').append(`<option value="${sala.id}">${sala.nombre}</option>`);
+}
 function buscarSala(id){
     return salas.find(sala => sala.id === id);
 }
